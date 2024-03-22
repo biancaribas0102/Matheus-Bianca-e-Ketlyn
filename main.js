f9f439fd6a433cc6f7bc810ab1fb554ecbd3560a
bianca ribas <126172203+biancaribas0102@users.noreply.github.com>
2024-03-22 08:58:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -24,11 +24,32 @@
 
 
  function calculatempo(tempoObjetivo) {
-
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = 
- }
- 
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(mnutos / 60)
+    let dias = Math.floor(horas / 24)
 
- 
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+     if (tempoFinal >0){
+         return  dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+     } else {
+          return "Prazo Finalizado";
+     }       
+ }
+
+ function atualizaCronometro(){
+     for  (let i=0; i<contadores.length;i++){
+           contadores[i].textContent = calculaTempo(tempos[i]);
+     }
+    }
+
+   function comecaCronometro(){
+       atualizaCronometro();
+       setInterval(atualizaCronometro,1000);
+   }
+   
+   comecaCronometro();
